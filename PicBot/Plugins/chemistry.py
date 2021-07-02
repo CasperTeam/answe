91d3c92c2b9bbdf3f609b7a76b7929ff9c5f9b3a
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-07-02 14:49:21 +0530

    Update chemistry.py

--- PicBot/Plugins/chemistry.py
+++ PicBot/Plugins/chemistry.py
@@ -3,10 +3,11 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
+
 @Client.on_message(filters.command(["chemistry"]))
 async def chemistry(bot, update):
 
-       source = requests.get("https://teletype.in/@satyendra/Maths").text
+       source = requests.get("https://teletype.in/@satyendra/Chemistry").text
        soup = BeautifulSoup(source, "lxml")
        text = "\n".join(_.text for _ in soup.find("article").find_all("p"))
-     await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
+       await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
