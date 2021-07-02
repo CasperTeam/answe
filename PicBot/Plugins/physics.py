a51a7e16b5c3b681427968c5e1c130556f09cb71
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-07-02 15:00:36 +0530

    Update physics.py

--- PicBot/Plugins/physics.py
+++ PicBot/Plugins/physics.py
@@ -5,7 +5,8 @@ import requests
 
 @Client.on_message(filters.command(["physics"]))
 async def physics(bot, update):
-     source = requests.get('https://teletype.in/@satyendra/Physics').text
-     soup = BeautifulSoup(source, 'lxml')
-     text = soup.article.text
-     await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
+
+       source = requests.get("https://teletype.in/@satyendra/Physics").text
+       soup = BeautifulSoup(source, "lxml")
+       text = "\n".join(_.text for _ in soup.find("article").find_all("p"))
+       await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
