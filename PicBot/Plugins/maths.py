9e6135be3a3190ee9d711482e6c41d11e3ce53b6
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-07-02 14:59:19 +0530

    Update maths.py

--- PicBot/Plugins/maths.py
+++ PicBot/Plugins/maths.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 
 @Client.on_message(filters.command(["maths"]))
 async def maths(bot, update):
-     source = requests.get('https://teletype.in/@satyendra/Maths').text
-     soup = BeautifulSoup(source, 'lxml')
-     text = soup.article.text
-     await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
+
+       source = requests.get("https://teletype.in/@satyendra/Maths").text
+       soup = BeautifulSoup(source, "lxml")
+       text = "\n".join(_.text for _ in soup.find("article").find_all("p"))
+       await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
