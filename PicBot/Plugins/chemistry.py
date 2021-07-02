61d3da819a87f5d5f5b74d7bdf2411e4707b87a8
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-07-02 15:10:31 +0530

    Update chemistry.py

--- PicBot/Plugins/chemistry.py
+++ PicBot/Plugins/chemistry.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
+import requests
 import re
 from bs4 import BeautifulSoup
-import requests
-
 
 @Client.on_message(filters.command(["chemistry"]))
 async def chemistry(bot, update):
