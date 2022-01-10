2a206f82196920add1649842f11607175491f21a
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-01-10 08:45:16 +0530

    Update all.py

--- PicBot/Plugins/all.py
+++ PicBot/Plugins/all.py
@@ -16,8 +16,17 @@ async def all(bot, update):
                 source3 = requests.get("https://teletype.in/@satyendra/Physics").text
                 i = BeautifulSoup(source3, "lxml")
                 satya3 = "\n".join(_.text for _ in i.find("article").find_all("p"))
-                text = (satya1+satya3+satya2)
-                await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
+                text = """
+                Physics
+                {}
+                
+                Chemistry
+                {}
+                
+                maths
+                {}
+                """
+                await bot.send_message(update.chat.id, text.format(satya1, satya2, satya3), reply_to_message_id=update.message_id)
         else:
          text="**Wait A Min**\n**Who Are You**"
          await bot.send_message(update.chat.id, text, reply_to_message_id=update.message_id)
